bd0a58c4cce01c7a92c6f5efce03cd1c3b6c1702
Albert <fualbert20030507@gmail.com>
2024-07-19 13:08:31 +0800

    7/19 Solved

---
+++ 2024/07/19/q1380.java
@@ -0,0 +1,8 @@
+// 1380. Lucky Numbers in a Matrix
+// https://leetcode.com/problems/lucky-numbers-in-a-matrix/description/
+
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+
+    }
+}
