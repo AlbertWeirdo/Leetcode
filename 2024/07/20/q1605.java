ef72d0aebc00874beab11b2f06a746df19500ae6
Albert <fualbert20030507@gmail.com>
2024-07-20 10:04:31 +0800

    include a better solution

--- 2024/07/20/q1605.java
+++ 2024/07/20/q1605.java
@@ -2,10 +2,13 @@
 // https://leetcode.com/problems/find-valid-matrix-given-row-and-column-sums/description/
 
 public class q1605 {
-    Solution s = new Solution();
-    int[] rowSum = { 3, 8 };
-    int[] colSum = { 4, 7 };
-    int[][] ans = s.restoreMatrix(rowSum, colSum);
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] rowSum = { 5, 7, 10 };
+        int[] colSum = { 8, 6, 8 };
+        int[][] ans = s.restoreMatrix(rowSum, colSum);
+    }
+
 }
 
 class Solution {
@@ -23,3 +26,31 @@ class Solution {
         return ans;
     }
 }
+
+// better solution
+/*
+ * class Solution {
+ * public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+ * int row = rowSum.length;
+ * int col = colSum.length;
+ * int[][] ans = new int[row][col];
+ * int i = 0, j = 0;
+ * 
+ * while (i < row && j < col) {
+ * int minValue = Math.min(rowSum[i], colSum[j]);
+ * ans[i][j] = minValue;
+ * rowSum[i] -= minValue;
+ * colSum[j] -= minValue;
+ * 
+ * if (rowSum[i] == 0) {
+ * i++;
+ * }
+ * if (colSum[j] == 0) {
+ * j++;
+ * }
+ * }
+ * 
+ * return ans;
+ * }
+ * }
+ */
