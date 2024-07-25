ace86e0ff22f754713cf649d0bd3aba951241cad
AlbertWeirdo <fualbert20030507@gmail.com>
2024-07-25 10:09:45 +0800

    Time: 43 ms (26.81%), Space: 55.9 MB (60.74%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
-        return nums;
+        Integer[] objNums=Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(objNums, (a,b)->Integer.compare(a,b));
+        return Arrays.stream(objNums).mapToInt(Integer::intValue).toArray();
     }
 }
