c6e10b503503d285d4a8d897be53fe2a984155ed
AlbertWeirdo <fualbert20030507@gmail.com>
2024-07-25 11:38:36 +0800

    Time: 0 ms (100%), Space: 41.7 MB (41.33%) - LeetHub

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    int i = 0;
+    public String reverseParentheses(String s) {
+        char[] ar = s.toCharArray();
+        return helper(ar);
+    }
+
+    public String helper(char[] s){
+        StringBuilder sb = new StringBuilder();
+
+        while(i < s.length){
+            if(s[i] == ')'){
+                i++;
+                return sb.reverse().toString();
+            }else if(s[i] == '('){
+                i++;
+                String st  = helper(s);
+                sb.append(st);
+            }else{
+                sb.append(s[i]);
+                i++;
+            }
+        }
+        return sb.toString();
+
+    }
+}
