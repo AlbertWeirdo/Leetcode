fbd0f5139ba7e16063a81bbded83e896504c53fe
AlbertWeirdo <fualbert20030507@gmail.com>
2024-07-30 12:18:49 +0800

    Time: 19 ms (92.48%), Space: 45.5 MB (39.55%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.java
@@ -1,18 +1,14 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int count_b = 0;
-        int min_deletions = 0;
-
-        for (char c : s.toCharArray()) {
-            if (c == 'a') {
-
-                min_deletions = Math.min(min_deletions + 1, count_b);
-            } else {
-
+        int count_b=0;
+        int min_deletion=0;
+        for (char c:s.toCharArray()){
+            if (c=='a'){
+                min_deletion=Math.min(min_deletion+1, count_b);
+            }else{
                 count_b++;
             }
         }
-
-        return min_deletions;
+        return min_deletion;
     }
 }
